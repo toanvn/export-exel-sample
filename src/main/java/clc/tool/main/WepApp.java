f61486889aa41757f520608efae0af3647e4627a
Tommy <vantoan23@gmail.com>
2016-11-22 10:13:55 +0700

    - Add log config

--- src/main/java/clc/tool/main/WepApp.java
+++ src/main/java/clc/tool/main/WepApp.java
@@ -1,8 +1,13 @@
 package clc.tool.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Created by qup on 11/21/16.
@@ -11,7 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WepApp {
 
+    static Logger logger = LoggerFactory.getLogger(WepApp.class);
+
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(WepApp.class, args);
+        ApplicationContext ctx = SpringApplication.run(WepApp.class, args);
+        logger.debug("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            logger.debug(beanName);
+        }
     }
 }
